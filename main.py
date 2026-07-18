27813ddb5abb2146c77033e67402396b8e5a1caa
naldydimpudus-18 <reinaldydimpudus@gmail.com>
2026-07-18 12:57:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from playwright.sync_api import sync_playwright
 
-URL = "https://www.google.com/travel/flights/search?tfs=CBwQAhokEgoyMDI2LTEyLTIxag0IAhIJL20vMDJuYmgxcgcIARIDR1RPQAFIAXABggELCP___________wGYAQI"
-
 with sync_playwright() as p:
+
     browser = p.chromium.launch(
         headless=True,
-        args=["--no-sandbox"]
+        args=[
+            "--disable-blink-features=AutomationControlled",
+            "--disable-dev-shm-usage",
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-gpu"
+        ]
     )
 
-    page = browser.new_page(
-        viewport={"width": 1600, "height": 1200}
+    context = browser.new_context(
+        locale="id-ID",
+        timezone_id="Asia/Jakarta",
+        viewport={"width": 1366, "height": 768},
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
     )
 
-    page.goto(URL, wait_until="networkidle", timeout=120000)
+    page = context.new_page()
 
-    page.screenshot(
-        path="flight.png",
-        full_page=True
-    )
-
-    print("=" * 50)
-    print(page.title())
-    print(page.url)
-    print("=" * 50)
-
-    browser.close()
+    page.set_extra_http_headers({
+        "Accept-Language": "id-ID,id;q=0.9,en;q=0.8"
+    })
