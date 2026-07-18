ff8591286903337671e3f93de4460dfd5b686683
naldydimpudus-18 <reinaldydimpudus@gmail.com>
2026-07-18 12:47:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from playwright.sync_api import sync_playwright
 
+URL = "https://www.google.com/travel/flights/search?tfs=CBwQAhokEgoyMDI2LTEyLTIxag0IAhIJL20vMDJuYmgxcgcIARIDR1RPQAFIAXABggELCP___________wGYAQI"
+
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
-    page.goto("https://www.google.com")
+    page.goto(URL, wait_until="networkidle")
 
-    print("PAGE TITLE:")
+    page.screenshot(path="flight.png", full_page=True)
+
     print(page.title())
 
     browser.close()
