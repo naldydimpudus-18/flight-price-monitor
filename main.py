ce72f6560e333b669d455bd7047e1ed8222ea1c8
naldydimpudus-18 <reinaldydimpudus@gmail.com>
2026-07-21 15:40:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,91 @@
 from playwright.sync_api import sync_playwright
+import gspread
+from google.oauth2.service_account import Credentials
+import os
+import json
 import re
+from datetime import datetime
 
-URL = "https://www.google.com/travel/flights/search?tfs=CBwQAhokEgoyMDI2LTEyLTIxag0IAhIJL20vMDJuYmgxcgcIARIDR1RPQAFIAXABggELCP___________wGYAQI"
+# =====================
+# GOOGLE SHEETS LOGIN
+# =====================
+
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    creds_dict,
+    scopes=scopes
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open_by_key(os.environ["SHEET_ID"])
+
+routes_ws = sheet.worksheet("Routes")
+pricelog_ws = sheet.worksheet("PriceLog")
+
+# =====================
+# GOOGLE FLIGHTS CHECK
+# =====================
+
+URLS = {
+    "DPS-GTO": "https://www.google.com/travel/flights/search?tfs=CBwQAhokEgoyMDI2LTEyLTIxag0IAhIJL20vMDJuYmgxcgcIARIDR1RPQAFIAXABggELCP___________wGYAQI",
+    "DPS-MDC": "https://www.google.com/travel/flights/search?tfs=CBwQAhokEgoyMDI2LTEyLTIxag0IAhIJL20vMDJuYmgxcgcIARIDTURDQAFIAXABggELCP___________wGYAQI&tfu=EgYIABAAGAA",
+    "DPS-PLW": "https://www.google.com/travel/flights/search?tfs=CBwQAhokEgoyMDI2LTEyLTIxag0IAhIJL20vMDJuYmgxcgcIARIDUExXQAFIAXABggELCP___________wGYAQI&tfu=EgYIABAAGAA"
+}
 
 with sync_playwright() as p:
+
     browser = p.chromium.launch(headless=True)
 
-    page = browser.new_page()
+    for route, url in URLS.items():
 
-    page.goto(URL, wait_until="networkidle", timeout=120000)
+        page = browser.new_page()
 
-    text = page.locator("body").inner_text()
+        print(f"Checking {route}")
 
-    match = re.search(r"\$(\d+)", text)
+        page.goto(
+            url,
+            wait_until="networkidle",
+            timeout=120000
+        )
 
-    if match:
-        print(f"PRICE FOUND: ${match.group(1)}")
-    else:
-        print("PRICE NOT FOUND")
+        text = page.locator("body").inner_text()
 
-    page.screenshot(path="google-flights.png", full_page=True)
+        match = re.search(r"\$(\d+)", text)
+
+        if match:
+
+            usd_price = int(match.group(1))
+
+            idr_price = usd_price * 16500
+
+            timestamp = datetime.now().strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+
+            pricelog_ws.append_row([
+                timestamp,
+                route,
+                usd_price,
+                idr_price
+            ])
+
+            print(
+                f"{route} : USD {usd_price} | IDR {idr_price}"
+            )
+
+        else:
+
+            print(
+                f"{route} : PRICE NOT FOUND"
+            )
+
+        page.close()
 
     browser.close()
