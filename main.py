f7817ecb4ab8f978dd9d440cc16300b12cd53ecd
naldydimpudus-18 <reinaldydimpudus@gmail.com>
2026-07-21 15:35:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from playwright.sync_api import sync_playwright
+import re
 
 URL = "https://www.google.com/travel/flights/search?tfs=CBwQAhokEgoyMDI2LTEyLTIxag0IAhIJL20vMDJuYmgxcgcIARIDR1RPQAFIAXABggELCP___________wGYAQI"
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(
-        headless=True
-    )
+    browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
     page.goto(URL, wait_until="networkidle", timeout=120000)
 
-    print("TITLE:")
-    print(page.title())
+    text = page.locator("body").inner_text()
+
+    match = re.search(r"\$(\d+)", text)
+
+    if match:
+        print(f"PRICE FOUND: ${match.group(1)}")
+    else:
+        print("PRICE NOT FOUND")
 
     page.screenshot(path="google-flights.png", full_page=True)
-
-    print("SCREENSHOT SAVED")
 
     browser.close()
